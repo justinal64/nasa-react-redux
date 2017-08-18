2b5985179f409ed585a5f2403565923d2db8d84d
justin leggett <justin@codingrookie.com>
2017-08-18 14:57:36 -0500

    added a random date counter, so I can pull in different days each time I refresh

--- src/app/components/AstronomyContainer.js
+++ src/app/components/AstronomyContainer.js
@@ -12,9 +12,23 @@ class AstronomyContainer extends Component {
     };
   }
 
-  componentDidMount() {
+  randomDate() {
+    const year = new Date().getFullYear() - 1; // current year minus 1
+    const month = Math.floor(Math.random() * 12) + 1; // random month between 1 and 12
+    const day = Math.floor(Math.random() * 25) + 1; // random day between 1 and 25
+    const randomTime = {
+      year,
+      month,
+      day
+    };
+    return randomTime;
+  }
+
+  queryForImage() {
+    const randomDate = this.randomDate();
+    console.log(randomDate);
     const API_KEY = "Yxr0aPO5Go1B1eAu8BYhZiR1tQXVMhNeXb4FqYHG";
-    const END_POINT = "https://api.nasa.gov/planetary/apod?api_key=";
+    const END_POINT = `https://api.nasa.gov/planetary/apod?date=${randomDate.year}-${randomDate.month}-${randomDate.day}&api_key=`;
 
     axios
       .get(END_POINT + API_KEY)
@@ -22,11 +36,13 @@ class AstronomyContainer extends Component {
         this.setState({
           astronomy: response.data
         });
-        console.log(this.state.astronomy);
       })
       .catch(error => {
         console.log(error, "failed to fetch data");
       });
+  }
+  componentDidMount() {
+    this.queryForImage();
   }
 
   render() {
