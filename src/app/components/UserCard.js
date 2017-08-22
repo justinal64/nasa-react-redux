86b83d377b45ce682a43356140db4ba0131637f1
justin leggett <justin@codingrookie.com>
2017-08-22 12:35:11 -0500

    mapping over data in UserCard.

--- src/app/components/UserCard.js
+++ src/app/components/UserCard.js
@@ -2,44 +2,42 @@ import React from "react";
 
 const UserCard = props => {
   function renderContent() {
-    switch (media_type) {
-      case "video":
-        return (
-          <iframe
-            allowFullScreen
-            frameBorder="0"
-            height="520"
-            width="720"
-            src={url}
-          />
-        );
-
-      case "image":
-        return <img src={url} alt={title} />;
-
-      default:
-        return null;
-    }
+    // switch (media_type) {
+    //   case "video":
+    //     return (
+    //       <iframe
+    //         allowFullScreen
+    //         frameBorder="0"
+    //         height="520"
+    //         width="720"
+    //         src={url}
+    //       />
+    //     );
+    //   case "image":
+    //     return <img src={url} alt={title} />;
+    //   default:
+    //     return null;
+    // }
   }
 
-  return <div>UserCard Page</div>;
+  const test = Object.keys(props.data).map((user, index) => {
+    return (
+      <p key={index}>
+        First Name: {props.data[index].name.first}
+        Last Name: {props.data[index].name.last}
+        Gender: {props.data[index].gender}
+        Phone Number: {props.data[index].phone}
+      </p>
+    );
+  });
+
+  return (
+    <div>
+      <ul>
+        {test}
+      </ul>
+    </div>
+  );
 };
 
 export default UserCard;
-
-// <div className="astronomy-card">
-// {renderContent()}
-// <div className="img-details">
-//   <h2 className="astronomy-title">
-//     {title}
-//   </h2>
-
-//   <p>
-//     {explanation}
-//   </p>
-
-//   <span>
-//     {date}, {copyright}
-//   </span>
-// </div>
-// </div>
