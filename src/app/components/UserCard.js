6dc85e7255742405ac8a3ff7c5b801a0ffcf8d13
justin leggett <justin@codingrookie.com>
2017-08-22 14:17:24 -0500

    displaying random user data

--- src/app/components/UserCard.js
+++ src/app/components/UserCard.js
@@ -1,43 +1,23 @@
 import React from "react";
 
-const UserCard = props => {
-  function renderContent() {
-    // switch (media_type) {
-    //   case "video":
-    //     return (
-    //       <iframe
-    //         allowFullScreen
-    //         frameBorder="0"
-    //         height="520"
-    //         width="720"
-    //         src={url}
-    //       />
-    //     );
-    //   case "image":
-    //     return <img src={url} alt={title} />;
-    //   default:
-    //     return null;
-    // }
-  }
-
-  const test = Object.keys(props.data).map((user, index) => {
+const DisplayUsers = props => {
+  const userData = Object.keys(props.data).map((user, index) => {
     return (
-      <p key={index}>
-        First Name: {props.data[index].name.first}
-        Last Name: {props.data[index].name.last}
+      <li key={index}>
+        Name: {props.data[index].name.first} {props.data[index].name.last}
         Gender: {props.data[index].gender}
         Phone Number: {props.data[index].phone}
-      </p>
+      </li>
     );
   });
 
   return (
     <div>
       <ul>
-        {test}
+        {userData}
       </ul>
     </div>
   );
 };
 
-export default UserCard;
+export default DisplayUsers;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import AppReducer from "./app_reducer";
 import RandomReducer from "./random_reducer";
+
 const rootReducer = combineReducers({
   astronomy: AppReducer,
   randomData: RandomReducer
